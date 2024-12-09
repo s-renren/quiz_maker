95c4fb0b2905c92dc77260fc24b6a0f599c5805c
s-renren <s1f102301156@iniad.org>
2024-12-09 14:17:21 +0900

    入力したらいったん表示でkる用に

--- client/features/works/Works.tsx
+++ client/features/works/Works.tsx
@@ -1,13 +1,68 @@
-import { useState } from 'react';
+import type { DtoId } from 'common/types/brandedId';
+import type { WorkEntity } from 'common/types/work';
+import { useCatchApiErr } from 'hooks/useCatchApiErr';
+import type { FormEvent } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { apiClient } from 'utils/apiClient';
 import styles from './works.module.css';
 
+type ContentDict = Record<DtoId['work'], string | undefined>;
+
+// const MainContent = (props: {work:WorkEntity;contentDict:ContentDict}) => {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// }
+
 export const Works = () => {
+  const [works, setWorks] = useState<WorkEntity[]>();
   const [quiz, setQuiz] = useState('');
   const [answer, setAnswer] = useState('');
+  const [contentDict, setContentDict] = useState<ContentDict>({});
+  const catchApiErr = useCatchApiErr();
+  const fetchContent = useCallback(async (w: WorkEntity) => {
+    const content = await fetch(w.quiz).then((b) => b.text());
+    setContentDict((dict) => ({ ...dict, [w.id]: content }));
+  }, []);
+
+  const createWork = async (e: FormEvent) => {
+    e.preventDefault();
+    setQuiz('');
+    setAnswer('');
+
+    const work = await apiClient.private.works
+      .$post({
+        body: {
+          quiz,
+          answer,
+        },
+      })
+      .catch(catchApiErr);
+
+    if (work !== null && works?.every((w) => w.id !== work.id)) {
+      setWorks((works) => [work, ...(works ?? [])]);
+    }
+  };
+
+  useEffect(() => {
+    if (works !== undefined) return;
+
+    apiClient.private.works
+      .$get()
+      .then((ws) => {
+        setWorks(ws);
+
+        return Promise.all(ws.map(fetchContent));
+      })
+      .catch(catchApiErr);
+  }, [catchApiErr, fetchContent, works, contentDict]);
+
   return (
     <div className={styles.main}>
       <div className={styles.makeQuiz}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={createWork}>
           <input
             value={quiz}
             className={styles.textInput}
@@ -27,6 +82,16 @@ export const Works = () => {
           </div>
         </form>
       </div>
+      {works?.map((work) => (
+        <div key={work.id} className={styles.makeQuiz}>
+          {/* <MainContent work={work} contentDict={contentDict} /> */}
+          <div className={styles.form}>
+            <div className={styles.title}>
+              <span>{work.quiz}</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
